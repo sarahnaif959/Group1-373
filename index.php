4d51cb4740f0589af9f965ecd2f32645dc74fd8a
sarahnaif959 <sarahamal2015@gmail.com>
2023-10-25 23:12:48 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+
+?>
+<!DOUCTYPE html>
+<html>
+<head>
+  <title>My website <title>
+</head>
+<body>
+
+  <a href="logout.php">Logout</a>
+  <h1>This is the index page <h1>
+
+  <br>
+   hello 
+</body>
+</html>
